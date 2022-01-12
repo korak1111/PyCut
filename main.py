32508f52618fae56f25cb06d3c082b2f82d16010
cyrus <cyrus.s.rupa@gmail.com>
2022-01-12 12:35:45 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import subprocess
+from time import sleep
+
+import pynput
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+
+def screen_cap():
+    keyboard.press(Key.alt)
+    keyboard.press('n')
+    keyboard.release(Key.alt)
+    keyboard.release('n')
+    return
+
+
+def file_save(name, counter):
+    save_name = name + "_" + str(counter)
+    keyboard.press(Key.ctrl)
+    keyboard.press('s')
+    keyboard.release(Key.ctrl)
+    keyboard.release('s')
+    sleep(1)
+    keyboard.type(save_name)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    counter += 1
+    return counter
+
+
+# location = input("Where do you want to save the files?")
+name = input("File name starts with: ")
+start = int(input("Start at: "))
+counter = start
+while True:
+    subprocess.Popen("C:\\Windows\\System32\\snippingTool.exe")
+    sleep(0.5)
+    screen_cap()
+    sleep(3)
+    counter = file_save(name, counter)
+    sleep(0.5)
+    keyboard.press(Key.cmd)
+    keyboard.press(Key.down)
+    keyboard.release(Key.cmd)
+    keyboard.release(Key.down)
+    sleep(1.5)
